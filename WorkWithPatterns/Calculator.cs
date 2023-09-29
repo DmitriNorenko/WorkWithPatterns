37f54780ef0fbf80f0598513b1de827904a5af32
Name <email@exaple.ru>
2023-09-29 08:45:36 +0300

    Создал интерфейс и классы

---
+++ WorkWithPatterns/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithPatterns
+{
+    public static class Calculator
+    {
+       
+        public static void CalculateInterest(Account account)
+        {
+            if (account.Type == "Обычный")
+            {
+                account.Interest = account.Balance * 0.4;
+
+                if (account.Balance < 1000)
+                    account.Interest -= account.Balance * 0.2;
+
+                if (account.Balance >= 1000)
+                    account.Interest -= account.Balance * 0.4;
+            }
+            else if (account.Type == "Зарплатный")
+            {
+                account.Interest = account.Balance * 0.5;
+            }
+        }
+    }
+}
